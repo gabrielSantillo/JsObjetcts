0c1a783a08536c95f6426da57271a7a5a9eb0bb9
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-09 14:52:39 -0600

    Ended the knoweledgecheck

--- js/app.js
+++ js/app.js
@@ -2,11 +2,43 @@ let company = {
     name: `Gabriel's Company`,
     yearly_revenue: 10000000,
     ceo: `Gabriel`,
-    number_of_employees: 87
+    number_of_employees: 87,
+    managers: [ {
+        name: `Alex`,
+        salary: 6000,
+        is_senior: true
+    }, 
+    {
+        name: `James`,
+        salary: 4000,
+        is_senior: true
+    },
+    {
+        name: `Hani`,
+        salary: 8000,
+        is_senior: false
+    }]
 }
 
 if (company[`yearly_revenue`] >= 1000000) {
     console.log(`Congratulations, ${company[`name`]}! ${company[`ceo`]}, have ran this business so good! They have managed a total of ${company[`number_of_employees`]} employees.`)
 } else {
     console.log("Work more!")
+}
+
+let counter_manager = 0;
+while(counter_manager < company[`managers`].length) {
+    console.log(company[`managers`][counter_manager]);
+    counter_manager ++;
+}
+
+for(let counter_manager = 0; counter_manager < company[`managers`].length; counter_manager++) {
+    console.log(company[`managers`][counter_manager]);
+}
+
+for(let counter_manager = 0; counter_manager < company[`managers`].length; counter_manager++) {
+    console.log(`Name of the manager: ${company[`managers`][counter_manager][`name`]}. Salary: ${company[`managers`][counter_manager][`salary`]}.`);
+    if (company[`managers`][counter_manager][`is_senior`] === true) {
+        console.log("Welcome!")
+    }
 }
