9365a6404535626e235b2d62d2f9f4adbbaa13a5
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-09 13:32:27 -0600

    Finished the knowledgecheck

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let company = {
+    name: `Gabriel's Company`,
+    yearly_revenue: 10000000,
+    ceo: `Gabriel`,
+    number_of_employees: 87
+}
+
+if (company[`yearly_revenue`] >= 1000000) {
+    console.log(`Congratulations, ${company[`name`]}! ${company[`ceo`]}, have ran this business so good! They have managed a total of ${company[`number_of_employees`]}.`)
+} else {
+    console.log("Work more!")
+}
