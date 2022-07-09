7c3358bdfe84cd78a4b41dadcf26c2f886738acf
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-09 13:39:01 -0600

    Added more thing to the console. Onde was missing a part of it

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let company = {
 }
 
 if (company[`yearly_revenue`] >= 1000000) {
-    console.log(`Congratulations, ${company[`name`]}! ${company[`ceo`]}, have ran this business so good! They have managed a total of ${company[`number_of_employees`]}.`)
+    console.log(`Congratulations, ${company[`name`]}! ${company[`ceo`]}, have ran this business so good! They have managed a total of ${company[`number_of_employees`]} employees.`)
 } else {
     console.log("Work more!")
 }
